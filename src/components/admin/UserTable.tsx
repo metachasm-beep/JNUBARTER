db76e8b0e35addd0bdb1990bfa8f41bdc60d422a
metachasm-beep <metachasm@gmail.com>
2026-04-23 03:43:10 +0530

    fix: add missing Search import to UserTable

--- src/components/admin/UserTable.tsx
+++ src/components/admin/UserTable.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { Search } from "lucide-react";
 import { UserRow } from "./UserRow";
 import { AdminUserDrawer } from "./AdminUserDrawer";
 
